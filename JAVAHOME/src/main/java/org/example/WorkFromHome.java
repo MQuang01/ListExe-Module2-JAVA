1a3cdd6669e865f4d15e650b3093854e404d12f2
Minh Quang <hcmquang2111@gmail.com>
2023-11-06 11:39:57 +0700

    workhome

---
+++ JAVAHOME/src/main/java/org/example/WorkFromHome.java
@@ -0,0 +1,77 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class WorkFromHome {
+
+    public WorkFromHome(){
+
+    }
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int number;
+//        boolean flag = false;
+/*
+        do {
+            System.out.println("Mời nhập từ 1-3: ");
+            number = scanner.nextInt();
+
+            if (number >= 1 && number <= 3){
+                flag = true;
+            }else {
+                System.out.println("Không hợp lệ");
+            }
+        }while (!flag);
+        scanner.close();
+
+        if(number == 1){
+            System.out.println("Bạn đang chọn Bia");
+        }
+        if(number == 2){
+            System.out.println("Bạn đang chọn Heo");
+        }
+        if(number == 3){
+            System.out.println("Bạn đang chọn Bò");
+        }
+*/
+        do {
+            System.out.println("Menu:");
+            System.out.println("1 : Bia");
+            System.out.println("2 : Heo");
+            System.out.println("3 : Bò");
+            System.out.println("Mời nhập từ 1-3: ");
+            number = scanner.nextInt();
+
+            switch (number)
+            {
+                case (1) :
+                    System.out.println("Bạn đang chọn Bia");
+                    break;
+                case (2) :
+                    System.out.println("Bạn đang chọn Heo");
+                    break;
+                case (3) :
+                    System.out.println("Bạn đang chọn Bò");
+                    break;
+                default :
+                    System.out.println("Không hợp lệ");
+            }
+        }while (number < 1 || number >3);
+
+
+
+//        int number = 0;
+//        do{
+//            System.out.println("Mời nhập: ");
+//            number = scanner.nextInt();
+//
+//        }while (number > 10);
+//
+//
+//        System.out.println("KẾT THÚC CHƯƠNG TRÌNH");
+
+
+    }
+}
